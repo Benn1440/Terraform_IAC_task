99f5083cede9f16fc9f956ee79b7ca38e2231f21
Benn1440 <benokafor67@gmail.com>
2024-07-24 09:48:54 +0100

    updated subnet AZx

--- modules/subnets/main.tf
+++ modules/subnets/main.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_Subnet" {
   vpc_id     = var.vpc_id
   cidr_block = "10.0.1.0/24"
-  availability_zone       = "us-west-1b"
+  availability_zone       = "eu-west-1b"
   map_public_ip_on_launch = true
 
   tags = {
